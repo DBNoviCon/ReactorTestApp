faefe5e3c84397169109c84c9bd55a709d6cdfc5
Deniz Böttcher <deniz.boettcher@novicon.net>
2022-04-27 15:07:23 +0200

    init

---
+++ DotReactorTestApp/DotReactorTestApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using DotReactorTestApp.ViewModels;
+using DotReactorTestApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DotReactorTestApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
